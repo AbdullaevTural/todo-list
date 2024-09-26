f5aff0dbf19c7e4073b93ae806444cf954a9c0a5
Tural <tural.abdullaev@mail.ru>
2024-09-26 06:16:49 +0400

    ToDoList

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,90 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-</template>
+  <div id="app">
+    <AppHeader />
 
+    <AppFilters :active-filter="activeFilter" @set-filter="setFilter" />
+
+    <main class="app-main">
+      <AppToDo :todos="filterdTodos" @toggle-todo="toggleTodo" @remove-todo="removeTodo" />
+
+      <AppAddToDo @add-todo="addToDo" />
+    </main>
+
+    <AppFooter :stats="stats" />
+  </div>
+</template>
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
-
+import AppHeader from './components/AppHeader.vue'
+import AppFilters from './components/AppFilters.vue'
+import AppToDo from './components/AppToDo.vue'
+import AppAddToDo from './components/AppAddToDo.vue'
+import AppFooter, { Stats } from './components/AppFooter.vue'
+import { Todo } from "@/types/Todo";
+import { Filter } from './types/Filter';
+interface State {
+  todos: Todo[],
+  activeFilter: Filter
+}
 export default defineComponent({
-  name: 'App',
   components: {
-    HelloWorld
+    AppHeader,
+    AppFilters,
+    AppToDo,
+    AppAddToDo,
+    AppFooter
+  },
+  data(): State {
+    return {
+      todos: this.loadTodos(),
+      activeFilter: 'All'
+    }
+  },
+  computed: {
+    filterdTodos(): Todo[] {
+      switch (this.activeFilter) {
+        case 'Active':
+          return this.todos.filter(todo => !todo.complited)
+        case 'Done':
+          return this.todos.filter(todo => todo.complited)
+        case 'All':
+        default:
+          return this.todos
+      }
+    },
+    stats(): Stats {
+      return {
+        active: this.todos.filter(todo => !todo.complited).length,
+        done: this.todos.filter(todo => todo.complited).length
+      }
+    }
+  },
+  methods: {
+    addToDo(todo: Todo) {
+      this.todos.push(todo),
+      this.saveTodos();
+    },
+    toggleTodo(id: number) {
+      const targetTodo = this.todos.find(todo => todo.id === id)
+      if (targetTodo) {
+        targetTodo.complited = !targetTodo.complited,
+        this.saveTodos();
+      }
+    },
+    removeTodo(id: number) {
+      this.todos = this.todos.filter((todo: Todo) => todo.id !== id),
+      this.saveTodos();
+    },
+    setFilter(filter: Filter) {
+      this.activeFilter = filter
+    },
+    saveTodos() {
+      localStorage.setItem('todos', JSON.stringify(this.todos));  // Сохраняем задачи в localStorage
+    },
+    loadTodos(): Todo[] {
+      const savedTodos = localStorage.getItem('todos');
+      return savedTodos ? JSON.parse(savedTodos) : [];
+    }
   }
 });
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/AppToDo.vue
@@ -0,0 +1,34 @@
+<template>
+  <ul class="todo-list">
+    <AppToDoItems v-for="todo in todos" :key="todo.id" :todo="todo" @toggle-todo="toggleTodo"
+      @remove-todo="removeTodo" />
+  </ul>
+</template>
+<script lang="ts">
+import { defineComponent, PropType } from "vue";
+import AppToDoItems from './AppToDoItems.vue'
+import { Todo } from "@/types/Todo";
+
+export default defineComponent({
+  components: {
+    AppToDoItems,
+  },
+  props: {
+    todos: {
+      type: Array as PropType<Todo[]>
+    }
+  },
+  methods: {
+    toggleTodo(id: number) {
+      this.$emit('toggleTodo', id)
+    },
+    removeTodo(id: number) {
+      this.$emit('removeTodo', id)
+    }
+  },
+  emits: {
+    toggleTodo: (id: number) => Number.isInteger(id),
+    removeTodo: (id: number) => Number.isInteger(id)
+  }
+})
+</script>
